517313c0cc6d9850b75f76b2037687637389a4a7
itisjustdiego <itisjustdiego@gmail.com>
2017-10-10 12:51:49 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
+
+
+  get "/newteam" do
+    erb :newteam
+    # binding.pry
+  end
+
+  post "/team" do
+    erb :team
+  end
 
 
 end
